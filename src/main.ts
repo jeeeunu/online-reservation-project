ac5c894cdf07eac83db45e3a531519bef1c047b8
jeeeunu <dbwldms25@gmail.com>
2023-08-05 03:45:54 +0900

    fix: main.ts - transform: true

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); // transform: true옵션을 넣으면 데이터 타입 변환을 해줌 (URI에 들어가는 파라미터는 항상 string이기에 사용함)
   app.use(cookieParser());
   await app.listen(3000);
 }
