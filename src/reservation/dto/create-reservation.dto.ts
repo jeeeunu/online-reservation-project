6a7391919c9828d83cce43d449ce23156653fcfd
jeeeunu <dbwldms25@gmail.com>
2023-08-05 00:43:35 +0900

    fix: create-reservation dto 수정

--- src/reservation/dto/create-reservation.dto.ts
+++ src/reservation/dto/create-reservation.dto.ts
@@ -1,7 +1,18 @@
+import { IsNumber } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsNumber()
   Perf_id: number;
+
+  @IsNumber()
   User_id: number;
+
+  @IsNumber()
   Seat_id: number;
+
+  @IsNumber()
   Perfd_id: number;
+
+  @IsNumber()
   price: number;
 }
