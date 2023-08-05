a68daa7a2562a21e8d8ac4fea0d772284097d2b5
jeeeunu <dbwldms25@gmail.com>
2023-08-06 02:06:42 +0900

    fix: dto IsNotEmpty 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,19 +1,22 @@
-import { IsString, IsOptional, IsBoolean } from 'class-validator';
+import { IsNotEmpty, IsString, IsOptional, IsBoolean } from 'class-validator';
 export class UserCreateDto {
+  @IsNotEmpty()
   @IsString()
   readonly user_email: string;
 
+  @IsNotEmpty()
   @IsString()
   user_password: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly user_name: string;
 
-  @IsString()
   @IsOptional()
+  @IsString()
   readonly user_image?: string;
 
-  @IsBoolean()
   @IsOptional()
+  @IsBoolean()
   readonly is_admin: boolean;
 }
